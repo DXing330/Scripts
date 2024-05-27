ff88e542c134daf71e95f19411903ec79112aa2a
Briky <draconyth@gmail.com>
2024-05-26 21:53:48 -0700

    MarketDataManager:

--- DataManagers/VillageData/MarketDataManager.cs
+++ DataManagers/VillageData/MarketDataManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class MarketDataManager : BasicDataManager
 {
+    public int basePrice = 50;
     public VillageDataManager villageData;
     // Generates equipment for the open market.
     public EquipmentGenerator equipmentGenerator;
@@ -17,8 +18,10 @@ public class MarketDataManager : BasicDataManager
     // Randomly new equipment may be available.
     public int lastUpdateDay;
     // If you buy out the equipment it will take some time to restock.
-    public List<string> availableEquipment;
-    public List<string> equipmentPrices;
+    public List<string> allAvailableEquipment;
+    public List<string> currentAvailable;
+    public List<string> currentQuantity;
+    public List<string> currentPrices;
     // Affects comissionable equipment and how many smiths you have.
     // Every smith can work on one equipment at a time.
     public List<BasicDataManager> ownedBusinesses;
@@ -36,8 +39,9 @@ public class MarketDataManager : BasicDataManager
         saveDataPath = Application.persistentDataPath;
         string data = "";
         data += marketLevel+"#"+marketInvestment+"#"+lastUpdateDay+"#";
-        data += GameManager.instance.ConvertListToString(availableEquipment, ",")+"#";
-        data += GameManager.instance.ConvertListToString(equipmentPrices)+"#";
+        data += GameManager.instance.ConvertListToString(currentAvailable, ",")+"#";
+        data += GameManager.instance.ConvertListToString(currentQuantity)+"#";
+        data += GameManager.instance.ConvertListToString(currentPrices)+"#";
         File.WriteAllText(saveDataPath+fileName, data);
         GameManager.instance.utility.DataManagerSave(ownedBusinesses);
     }
@@ -52,10 +56,12 @@ public class MarketDataManager : BasicDataManager
             marketLevel = int.Parse(blocks[0]);
             marketInvestment = int.Parse(blocks[1]);
             lastUpdateDay = int.Parse(blocks[2]);
-            availableEquipment = blocks[3].Split(",").ToList();
-            equipmentPrices = blocks[4].Split("|").ToList();
-            GameManager.instance.utility.RemoveEmptyListItems(availableEquipment, 0);
-            GameManager.instance.utility.RemoveEmptyListItems(equipmentPrices, 0);
+            currentAvailable = blocks[3].Split(",").ToList();
+            currentQuantity = blocks[4].Split("|").ToList();
+            currentPrices = blocks[4].Split("|").ToList();
+            GameManager.instance.utility.RemoveEmptyListItems(currentAvailable, 0);
+            GameManager.instance.utility.RemoveEmptyListItems(currentQuantity, 0);
+            GameManager.instance.utility.RemoveEmptyListItems(currentPrices, 0);
         }
         else
         {
@@ -67,25 +73,90 @@ public class MarketDataManager : BasicDataManager
     // Generate equipment with rarity based on the market level.
     public void GenerateRandomEquipment()
     {
+        int timeDifference = 0;
         // Market updates daily I guess?
         // Check the time first to see if you should generate equipment.
         if (GameManager.instance.time > lastUpdateDay)
         {
+            timeDifference = GameManager.instance.time - lastUpdateDay;
             lastUpdateDay = GameManager.instance.time;
-            availableEquipment.Clear();
-            equipmentPrices.Clear();
         }
         else {return;}
         int typeRng = 0;
         int levelRng = 0;
-        for (int i = 0; i < marketLevel; i++)
+        int sqrtLevel = GameManager.instance.utility.FloorSqrt(marketLevel);
+        for (int i = 0; i < timeDifference; i++)
         {
             typeRng = Random.Range(0, regularEquipment.Count);
-            levelRng = Random.Range(0, GameManager.instance.utility.FloorSqrt(marketLevel));
-            availableEquipment.Add(equipmentGenerator.GenerateEquipment(levelRng, regularEquipment[typeRng]));
-            // Need to generate a cost for the equipment.
-            equipmentPrices.Add(((levelRng+1)*(levelRng+1)*50 + Random.Range(-50,50)).ToString());
+            levelRng = Random.Range(0, Random.Range(1,sqrtLevel+1));
+            string newEquip = equipmentGenerator.GenerateEquipment(levelRng, regularEquipment[typeRng]);
+            int newPrice = GeneratePriceForEquipment(newEquip, levelRng);
+            for (int j = 0; j < Random.Range(1,sqrtLevel+1); j++)
+            {
+                int indexOf = currentAvailable.IndexOf(newEquip);
+                if (indexOf >= 0)
+                {
+                    AdjustQuantity(indexOf);
+                    AdjustPrice(indexOf, newPrice);
+                }
+                else
+                {
+                    currentAvailable.Add(newEquip);
+                    currentQuantity.Add("1");
+                    currentPrices.Add(newPrice.ToString());
+                }
+            }
         }
+    }
+
+    protected int GeneratePriceForEquipment(string equip, int rarity = 0, int volatility = 0)
+    {
+        int price = 0;
+        // Increase price for rarity.
+        price += (rarity+1)*(rarity+1)*basePrice;
+        // Increase price for special abilities.
+        string[] equipStats = equip.Split("|");
+        // 0-1 abilities is free.
+        int baseAbilities = GameManager.instance.utility.CountOccurencesOfCharInString(equipStats[5], ',')+GameManager.instance.utility.CountOccurencesOfCharInString(equipStats[6], ',');
+        if (baseAbilities <= 0)
+        {
+            if (equipStats[5].Length > 1 || equipStats[6].Length > 1){baseAbilities++;}
+        }
+        int rareAbilities = GameManager.instance.utility.CountOccurencesOfCharInString(equipStats[7], ',')+GameManager.instance.utility.CountOccurencesOfCharInString(equipStats[8], ',');
+        if (rareAbilities <= 0)
+        {
+            if (equipStats[7].Length > 1){rareAbilities++;}
+            if (equipStats[8].Length > 1){rareAbilities++;}
+        }
+        int totalAbilites = baseAbilities+(2*rareAbilities);
+        price += totalAbilites*basePrice*(rarity+1);
+        if (volatility > 0)
+        {
+            int maxVolatility = Mathf.Max(volatility, price*volatility/100);
+            price += Random.Range(-maxVolatility, maxVolatility);
+        }
+        return price;
+    }
+
+    protected void AdjustQuantity(int index, int quantity = 1)
+    {
+        if (index >= currentQuantity.Count || index < 0){return;}
+        int newQuantity = int.Parse(currentQuantity[index])+quantity;
+        if (newQuantity <= 0)
+        {
+            currentAvailable.RemoveAt(index);
+            currentQuantity.RemoveAt(index);
+            currentPrices.RemoveAt(index);
+            return;
+        }
+        currentQuantity[index] = (newQuantity).ToString();
+    }
+
+    protected void AdjustPrice(int index, int price = -1)
+    {
+        if (index >= currentPrices.Count || index < 0 || price <= 0){return;}
+        int averagePrice = (int.Parse(currentPrices[index])+price)/2;
+        currentPrices[index] = averagePrice.ToString();
     }
 
     public void BuyEquipment(int index)
---
+++ UI/Market/MarketPanelGUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketPanelGUI : MonoBehaviour
+{
+    // List of equipment.
+    public virtual void ActivatePanel()
+    {
+
+    }
+
+    public virtual void DeactivatePanel()
+    {
+
+    }
+}
